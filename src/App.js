5786fc333de9e313d30294da5fcad0482add827c
Rohit Agharkar <rohitsa40@gmail.com>
2020-09-14 08:46:08 +0530

    Added Details page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
-import { render } from "react-dom";
-import SearchParams from "./SearchParams";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchParams from './SearchParams';
+import { Link, Router } from '@reach/router';
+import Details from './Details';
 
 const App = () => {
   return (
-    <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </React.StrictMode>
   );
 };
-render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
